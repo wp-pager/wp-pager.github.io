be8019195aa69eee94578f756a1d6ad7140867dd
SerhiiCho <serhiicho@protonmail.com>
2023-04-06 19:11:14 +0300

    Changed footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@ import Author from '@/components/Author.vue'
 </script>
 
 <template>
-    <footer class="py-12 lg:py-24 bg-gray-800 text-center">
+    <footer class="py-10 lg:py-16 bg-gradient-to-b from-gray-900 to-gray-800 text-center">
         <Author />
     </footer>
 </template>
