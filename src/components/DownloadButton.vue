b415d5bd076f773d808151464e5cd9c7f61df6d1
SerhiiCho <serhiicho@protonmail.com>
2025-07-09 19:18:01 +0300

    fix: download linik

--- src/components/DownloadButton.vue
+++ src/components/DownloadButton.vue
@@ -2,7 +2,10 @@
 import DownloadIcon from '@/components/Icons/DownloadIcon.vue'
 
 function download(): void {
-    window.open('https://github.com/wp-pager/wp-pager/archive/refs/heads/master.zip', '_blank')
+    window.open(
+        'https://github.com/wp-pager/wp-pager/archive/refs/heads/master.zip',
+        '_blank',
+    )
 }
 </script>
 
