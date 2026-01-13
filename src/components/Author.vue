3d331d6bf8ab9d2525bf461524f0ef19ea446232
SerhiiCho <serhiicho@protonmail.com>
2026-01-13 10:12:59 +0200

    refactor: Author.vue component

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -8,11 +8,16 @@ window.setInterval(() => enabled.value = !enabled.value, 2000)
 
 <template>
     <a
-        href="https://serhii.io/about-me"
+        href="https://serhiicho.com/about-me"
         target="_blank"
         class="text-center inline-block transition-all hover:-translate-y-2 duration-500 relative group"
     >
-        <span class="block text-gray-400 tracking-widest text-xs lg:text-sm transition-all duration-500 drop-shadow-[2px_8px_2px_rgb(0_0_0/50%)] group-hover:-translate-y-2 group-hover:scale-90 group-hover:drop-shadow-[2px_5px_2px_rgb(0_0_0/50%)]">
+        <span :class="[
+            'block text-gray-400 tracking-widest text-xs lg:text-sm transition-all',
+            'duration-500 drop-shadow-[2px_8px_2px_rgb(0_0_0/50%)]',
+            'group-hover:-translate-y-2 group-hover:scale-90',
+            'group-hover:drop-shadow-[2px_5px_2px_rgb(0_0_0/50%)]',
+        ]">
             <span class="mr-1">MADE</span>
             <span
                 class="transition-all duration-1000"
@@ -33,10 +38,23 @@ window.setInterval(() => enabled.value = !enabled.value, 2000)
         </span>
 
         <div class="inline-flex font-bold tracking-widest text-sm lg:text-md relative">
-            <div class="absolute h-[1px] -left-12 top-1/2 w-8 delay-200 group-hover:w-24 bg-white group-hover:opacity-30 group-hover:translate-x-[-4px] group-hover:-translate-y-2.5 transition-all duration-1000"></div>
-            <div class="absolute h-[1px] -right-12 top-1/2 w-8 delay-200 group-hover:w-24 bg-white group-hover:opacity-30 group-hover:translate-x-[5px] group-hover:-translate-y-2.5 transition-all duration-1000"></div>
+            <div :class="[
+                'absolute h-px -left-12 top-1/2 w-8 delay-200 group-hover:w-24',
+                'bg-white group-hover:opacity-30 group-hover:-translate-x-1',
+                'group-hover:-translate-y-2.5 transition-all duration-1000',
+            ]"></div>
+            <div :class="[
+                'absolute h-px -right-12 top-1/2 w-8 delay-200 group-hover:w-24',
+                'bg-white group-hover:opacity-30 group-hover:translate-x-[5px]',
+                'group-hover:-translate-y-2.5 transition-all duration-1000',
+            ]"></div>
 
-            <h3 class="text-white relative font-sans group-hover:translate-y-2 transition-all duration-700 group-hover:scale-150 drop-shadow-[2px_8px_2px_rgb(0_0_0/50%)] group-hover:drop-shadow-[8px_10px_2px_rgb(0_0_0/50%)]">
+            <h3 :class="[
+                'text-white relative font-sans group-hover:translate-y-2',
+                'transition-all duration-700 group-hover:scale-150',
+                'drop-shadow-[2px_8px_2px_rgb(0_0_0/50%)]',
+                'group-hover:drop-shadow-[8px_10px_2px_rgb(0_0_0/50%)]',
+            ]">
                 &#123;S&#125; SERHII
             </h3>
         </div>
